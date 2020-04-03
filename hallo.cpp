6cae174f1bb69973f78f90cc9a1b9c6785473f45
Tsubasa-Komiyama <komiyama-tsubasa-vw@ynu.jp>
2020-04-03 15:19:22 +0900

    Update hallo.cpp

--- hallo.cpp
+++ hallo.cpp
@@ -5,6 +5,10 @@ using namespace std;
 int main(){
 
 
-    cout << "hello world!" << endl;
+    int sum = 0;
+    for (int i = 0; i < 10; i++){
+        sum += i+1;
+    }
+    cout << sum << endl;
     return 0;
 }
